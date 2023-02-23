4bf6bb05d3bc4110b6b36a06beec74a6938bfd82
Marie-France Han <mf212mf@gmail.com>
2023-02-22 20:03:19 -0500

    remove axios references

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,11 @@ import About from './About'
 import Bio from './Bio'
 import Header from './Header'
 import Projects from './Projects'
-import axios from 'axios'
-import {
-    BrowserRouter as Router,
-    Route, Routes, Link, BrowserRouter
-} from 'react-router-dom'
+// import axios from 'axios'
+// import {
+//     BrowserRouter as Router,
+//     Route, Routes, Link, BrowserRouter
+// } from 'react-router-dom'
 
 //old style: class Main extends React.Component {}
 
@@ -28,15 +28,15 @@ function Main() {
 
 
 
-    const buildProjects = async () => {
-        const response = await axios.get(myProjectsUrl)
-        let data = response.data
-        console.log('this is the data', data)
-        //there won't be a setstate
-        //you need to format the data so that it's compatible with what's inside projects.js
-        //and then use setElements
-        //what is the barest bone function so I can see the data on the Browser
-    }
+    // const buildProjects = async () => {
+    //     const response = await axios.get(myProjectsUrl)
+    //     let data = response.data
+    //     console.log('this is the data', data)
+    //     //there won't be a setstate
+    //     //you need to format the data so that it's compatible with what's inside projects.js
+    //     //and then use setElements
+    //     //what is the barest bone function so I can see the data on the Browser
+    // }
 
     // buildProjects();
 
